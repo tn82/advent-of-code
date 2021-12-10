b3f7d760623dfb392fe93cc4d69207da9a0a41dd
tn82 <tomas.thurin@gmail.com>
2021-12-10 08:27:21 +0100

    AOC-2021 Day10 clean up

--- 2021/10.py
+++ 2021/10.py
@@ -1,4 +1,11 @@
 
+def lists_to_dict(l1, l2):
+    res = {}
+    for a, b in zip(l1, l2):
+        res[a] = b
+        res[b] = a
+    return res
+
 def day_10():
     file = open('10.txt', 'r')
     input = []
@@ -8,51 +15,31 @@ def day_10():
     part1_sum = 0
     open_chars = "([{<"
     close_chars = ")]}>"
-    close_map = {}
-    close_map[")"] = "("
-    close_map["]"] = "["
-    close_map["}"] = "{"
-    close_map[">"] = "<"
-    open_map = {}
-    open_map["("] = ")"
-    open_map["["] = "]"
-    open_map["{"] = "}"
-    open_map["<"] = ">"
-    closep_map = {}
-    closep_map[")"] = 3
-    closep_map["]"] = 57
-    closep_map["}"] = 1197
-    closep_map[">"] = 25137
-    
-    close2_map = {}
-    close2_map[")"] = 1
-    close2_map["]"] = 2
-    close2_map["}"] = 3
-    close2_map[">"] = 4
-    input2 = []
+    chars_map = lists_to_dict(open_chars, close_chars)
+    part1_points_map = lists_to_dict(close_chars, [3, 57, 1197, 25137])
+    part2_points_map = lists_to_dict(close_chars, [1, 2, 3, 4])
     part2_points = []
     for row in input:
-        s = ""
+        parsed = ""
         good = True
         for c in row:
             if c in open_chars:
-                s = s + c
+                parsed = parsed + c
             if c in close_chars:
-                if not s:
-                    part1_sum += closep_map[c]
+                if not parsed:
+                    part1_sum += part1_points_map[c]
                     good = False
                     break
-                if close_map[c] != s[-1]:
-                    part1_sum += closep_map[c]
+                if chars_map[c] != parsed[-1]:
+                    part1_sum += part1_points_map[c]
                     good = False
                     break
                 else:
-                    s = s[:-1]
+                    parsed = parsed[:-1]
         if good:
-            input2.append(row)
             points = 0
-            for c2 in reversed(s):
-                points = points * 5 + close2_map[open_map[c2]]
+            for c in reversed(parsed):
+                points = points * 5 + part2_points_map[chars_map[c]]
             part2_points.append(points)
 
     print(f"Part one: {part1_sum}")

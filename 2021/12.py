2c6f8a29225b3aabdafd4653d7d426018b248eab
tn82 <tomas.thurin@gmail.com>
2021-12-12 15:57:18 +0100

    COA-2021 Day12 clean-up

--- 2021/12.py
+++ 2021/12.py
@@ -1,40 +1,24 @@
 from collections import defaultdict
 
-def find_all_paths(graph, start, end, path=[]):
+def find_paths(graph, start, end, lower_visited_twice = False, path=[]):
     path = path + [start]
+    if start.islower() and path.count(start) > 1:
+        lower_visited_twice = True
     if start == end:
         return [path]
     if start not in graph:
         return []
     paths = []
     for node in graph[start]:
-        if node not in path or node.isupper():
-            newpaths = find_all_paths(graph, node, end, path)
-            for newpath in newpaths:
-                paths.append(newpath)
-    return paths
-
-def find_all_paths2(graph, start, end, path=[], found2 = False):
-    path = path + [start]
-    
-    if not start.isupper() and path.count(start) == 2:
-        found2 = True
-
-    if start == end:
-        return [path]
-    if start not in graph:
-        return []
-    paths = []
-    for node in graph[start]:
-        if path.count(node) < 2 or node.isupper():
-            if not node.isupper() and found2 and node in path:
-                continue
-            if node == "start" and node in path:
-                continue
-            #print(path, node, path.count(node), found2)
-            newpaths = find_all_paths2(graph, node, end, path, found2)
-            for newpath in newpaths:
-                paths.append(newpath)
+        if node.islower() and path.count(node) > 1:
+            continue
+        if node.islower() and lower_visited_twice and node in path:
+            continue
+        if node == "start":
+            continue
+        newpaths = find_paths(graph, node, end, lower_visited_twice, path)
+        for newpath in newpaths:
+            paths.append(newpath)
     return paths
 
 def day_12():
@@ -43,11 +27,11 @@ def day_12():
     for line in file:
         s = line.strip().split("-")
         graph[s[0]].append(s[1])
-        graph[s[1]].append(s[0])
-    #graph = dict(graph)
-    paths = find_all_paths(graph, "start", "end")
+        graph[s[1]].append(s[0]) # Bi-directional graph
+        
+    paths = find_paths(graph, "start", "end", True)
     print(f"Part one: {len(paths)}") # 5254 correct
-    paths = find_all_paths2(graph, "start", "end")
+    paths = find_paths(graph, "start", "end", False)
     print(f"Part two: {len(paths)}") # 149385 correct
 
 day_12()

819c637608670e7667c98490da44d49782f1b3b9
tn82 <tomas.thurin@gmail.com>
2021-12-13 12:18:58 +0100

    AOC-2021 Day13 clean-up

--- 2021/13.py
+++ 2021/13.py
@@ -1,5 +1,16 @@
 from operator import itemgetter
 
+def ascii_print(grid):
+    max_x = max(grid, key=itemgetter(0))[0]
+    max_y = max(grid, key=itemgetter(1))[1]
+    plot = [[" "] * (max_x + 1) for _ in range(max_y + 1)]
+    for coo in grid:
+        plot[coo[1]][coo[0]] = "#"
+    for row in plot:
+        for val in row:
+            print(val, end='')
+        print()
+
 def day_13():
     file = open('13.txt', 'r')
     grid = {}
@@ -10,42 +21,31 @@ def day_13():
             continue
         if len(row) == 1:
             cut = row[0].replace('fold along ', '').split("=")
-            cuts.append(cut)
+            cuts.append([cut[0], int(cut[1])])
         else:
             coo = [int(i) for i in row]
             grid[tuple(coo)] = 1
 
+    print_part1 = True
     for cut in cuts:
         grid_cut = {}
-        c = int(cut[1])
         if cut[0] == "y":
             for coo in grid:
-                if coo[1] < c:
+                if coo[1] < cut[1]:
                     grid_cut[coo] = 1
-                if coo[1] > c:
-                    grid_cut[coo[0], c * 2 - coo[1]] = 1
+                if coo[1] > cut[1]:
+                    grid_cut[coo[0], cut[1] * 2 - coo[1]] = 1
         if cut[0] == "x":
             for coo in grid:
-                if coo[0] < c:
+                if coo[0] < cut[1]:
                     grid_cut[coo] = 1
-                if coo[0] > c:
-                    grid_cut[c * 2 - coo[0], coo[1]] = 1
+                if coo[0] > cut[1]:
+                    grid_cut[cut[1] * 2 - coo[0], coo[1]] = 1
         grid = grid_cut
-    print(f"Part one: {len(grid)}")
-
-    max_x = max(grid, key=itemgetter(0))[0]
-    max_y = max(grid, key=itemgetter(1))[1]
-    plot = [["."] * (max_x + 1) for _ in range(max_y + 1)]
-    print(f"Part two:")
-    x = []
-    y = []
-    for coo in grid:
-        y.append(-coo[1])
-        x.append(coo[0])
-        plot[coo[1]][coo[0]] = "#"
-    for row in plot:
-        for val in row:
-            print(val, end='')
-        print()
+        if print_part1:
+            print(f"Part one: {len(grid)}")
+            print_part1 = False
+    print("Part two:")
+    ascii_print(grid)
 
 day_13()

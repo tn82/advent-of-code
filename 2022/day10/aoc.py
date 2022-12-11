2eb9c5cc54ead8291bb47dcbb94286095fe72c9b
Tomas Thurin <tomas.thurin@gmail.com>
2022-12-11 13:34:44 +0100

    AOC-2022 Day10 refactor2

--- 2022/day10/aoc.py
+++ 2022/day10/aoc.py
@@ -10,80 +10,40 @@ def input_test():
 def check_signal(c: int):
     return c == 20 or c == 60 or c == 100 or c == 140 or c == 180 or c == 220
 
-def ctr(X, pixel):
-    return X - 1 <= pixel <= X + 1
+def update_cyle(X, cycle, pixel, CTR):
+    if X - 1 <= pixel <= X + 1:
+        CTR += "#"
+    else:
+        CTR += "."
+    cycle += 1
+    pixel += 1
+    if pixel > 39:
+        pixel = 0
+        CTR += "\n"
 
-def part_one():
+    return cycle, pixel, CTR
+
+def day10():
     X = 1
-    c = 1
-    count = 0
-    for line in input():
-        com = line.strip().split()
-        if com[0] == "noop":
-            addx = 0
-            c += 1
-            if check_signal(c):
-                count += c * X
-        else:
-            addx = int(com[1])
-            c += 1
-            if check_signal(c):
-                count += c * X
-            X += addx
-            c += 1
-            if check_signal(c):
-                count += c * X
-
-    print("Part 1: ", count)
-    assert count == 15020
-
-    X = 1
-    c = 1
+    cycle = 1
     count = 0
     pixel = 0
     CTR = ""
     for line in input():
         com = line.strip().split()
         if com[0] == "noop":
-            if ctr(X, pixel):
-                CTR += "#"
-            else:
-                CTR += "."
-            c += 1
-            pixel += 1
-            if pixel > 39:
-                pixel = 0
-                CTR += "\n"
-            if check_signal(c):
-                count += c * X
-            
+            cycle, pixel, CTR = update_cyle(X, cycle, pixel, CTR)
+            if check_signal(cycle):
+                count += cycle * X
         else:
             addx = int(com[1])
-            
-            if ctr(X, pixel):
-                CTR += "#"
-            else:
-                CTR += "."
-            c += 1
-            pixel += 1
-            if pixel > 39:
-                pixel = 0
-                CTR += "\n"
-            if check_signal(c):
-                count += c * X
-
-            if ctr(X, pixel):
-                CTR += "#"
-            else:
-                CTR += "."
-            c += 1
-            pixel += 1
-            if pixel > 39:
-                pixel = 0
-                CTR += "\n"
-            if check_signal(c):
-                count += c * X
+            cycle, pixel, CTR = update_cyle(X, cycle, pixel, CTR)
+            if check_signal(cycle):
+                count += cycle * X
+            cycle, pixel, CTR = update_cyle(X, cycle, pixel, CTR)
             X += addx
+            if check_signal(cycle):
+                count += cycle * X
 
 
 
@@ -94,4 +54,4 @@ def part_one():
     print("Part 2: ", "EFUGLPAP")
 
 
-part_one()
+day10()

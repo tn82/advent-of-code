b27f832498527b95a0c3928854a74c3bf39543fa
Tomas Thurin <tomas.thurin@gmail.com>
2022-12-11 13:24:35 +0100

    AOC-2022 Day10 refactor1

--- 2022/day10/aoc.py
+++ 2022/day10/aoc.py
@@ -7,11 +7,11 @@ def input_test():
         return [line.rstrip() for line in file]
 
 
-def printer(c: int):
+def check_signal(c: int):
     return c == 20 or c == 60 or c == 100 or c == 140 or c == 180 or c == 220
 
-def ctr(X, c):
-    return X <= c - 1 <= X + 2
+def ctr(X, pixel):
+    return X - 1 <= pixel <= X + 1
 
 def part_one():
     X = 1
@@ -22,60 +22,73 @@ def part_one():
         if com[0] == "noop":
             addx = 0
             c += 1
-            if printer(c):
+            if check_signal(c):
                 count += c * X
         else:
             addx = int(com[1])
             c += 1
-            if printer(c):
+            if check_signal(c):
                 count += c * X
             X += addx
             c += 1
-            if printer(c):
+            if check_signal(c):
                 count += c * X
 
     print("Part 1: ", count)
     assert count == 15020
 
     X = 1
-    c = 0
+    c = 1
+    count = 0
+    pixel = 0
     CTR = ""
-    for line in input_test():
+    for line in input():
         com = line.strip().split()
         if com[0] == "noop":
-            c += 1
-            if ctr(X, c):
+            if ctr(X, pixel):
                 CTR += "#"
             else:
-                CTR += " "
-            if c > 40:
-                c = 0
+                CTR += "."
+            c += 1
+            pixel += 1
+            if pixel > 39:
+                pixel = 0
                 CTR += "\n"
+            if check_signal(c):
+                count += c * X
             
         else:
             addx = int(com[1])
-
-            c += 1
-            if c > 40:
-                c = 0
-                CTR += "\n"
-            if ctr(X, c):
+            
+            if ctr(X, pixel):
                 CTR += "#"
             else:
-                CTR += " "
-
+                CTR += "."
             c += 1
-            if c > 40:
-                c = 0
+            pixel += 1
+            if pixel > 39:
+                pixel = 0
                 CTR += "\n"
-            if ctr(X, c):
+            if check_signal(c):
+                count += c * X
+
+            if ctr(X, pixel):
                 CTR += "#"
             else:
-                CTR += " "
+                CTR += "."
+            c += 1
+            pixel += 1
+            if pixel > 39:
+                pixel = 0
+                CTR += "\n"
+            if check_signal(c):
+                count += c * X
             X += addx
 
 
 
+    print("Part 1: ", count)
+    assert count == 15020
 
     print(CTR) # EFUGLPAP
     print("Part 2: ", "EFUGLPAP")

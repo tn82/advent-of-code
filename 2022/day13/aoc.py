61942daec0de125ff0697fa11f2aad293f5b1fa0
Tomas Thurin <tomas.thurin@gmail.com>
2022-12-13 15:54:54 +0100

    AOC-2022 Day13 refactor2

--- 2022/day13/aoc.py
+++ 2022/day13/aoc.py
@@ -1,86 +1,41 @@
+import functools
+import copy
+
+
 def input():
     with open("input.txt", "r") as file:
         return [line.rstrip() for line in file]
+
 
 def input_test():
     with open("test.txt", "r") as file:
         return [line.rstrip() for line in file]
 
 
-def compare(left, right):
-    while True:
-        if left and not right or type(left) == int and type(right) == list:
-            return 0
-        elif not left and right or type(right) == int and type(left) == list:
-            return 1
-        elif left == right:
-            return -1
-        elif type(left) == int and type(right) == int:
-            return 1 if left < right else 0
-        elif type(left) == list and type(right) != list:
-            right = [right]
-        elif type(left) != list and type(right) == list:
-            left = [left]
-        left0 = left.pop(0)
-        right0 = right.pop(0)
-        comp0 = compare(left0, right0)
-        if comp0 > -1:
-            return comp0
-
-def compare2(left, right):
+def compare(left_orig, right_orig):
+    left = copy.deepcopy(left_orig)
+    right = copy.deepcopy(right_orig)
     while True:
         if type(left) == int and type(right) == int:
             if left == right:
                 return 0
             return -1 if left < right else 1
-        elif type(left) == list and type(right) == list:
-            if left == right:
-                return 0
-            if left and not right:
-                return 1
-            elif not left and right:
-                return -1
-            left0 = left.pop(0)
-            right0 = right.pop(0)
-            comp0 = compare2(left0, right0)
-            if comp0:
-                return comp0
         elif type(left) == list and type(right) != list:
             right = [right]
-            if left == right:
-                return 0
-            if left and not right:
-                return 1
-            elif not left and right:
-                return -1
-            left0 = left.pop(0)
-            right0 = right.pop(0)
-            comp0 = compare2(left0, right0)
-            if comp0:
-                return comp0
         elif type(left) != list and type(right) == list:
             left = [left]
-            if left == right:
-                return 0
-            if left and not right:
-                return 1
-            elif not left and right:
-                return -1
-            left0 = left.pop(0)
-            right0 = right.pop(0)
-            comp0 = compare2(left0, right0)
-            if comp0:
-                return comp0
-import copy
-def compare3(left, right):
-    l = copy.deepcopy(left)
-    r = copy.deepcopy(right)
-    c = compare2(l, r)
-    if c == 1:
-        return -1
-    if c == 0:
-        return 1
-    return 0
+        if left == right:
+            return 0
+        if left and not right:
+            return 1
+        elif not left and right:
+            return -1
+        left0 = left.pop(0)
+        right0 = right.pop(0)
+        comp0 = compare(left0, right0)
+        if comp0:
+            return comp0
+
 
 def part_one():
     count = 0
@@ -98,21 +53,19 @@ def part_one():
         else:
             package += 1
             right = l
-            if compare2(left, right) < 0:
+            if compare(left, right) < 0:
                 count += package
 
 
     print("Part 1: ", count)
     assert count == 6420
 
+
 part_one()
 
-import functools
-compare_key = functools.cmp_to_key(compare3)
 
 def part_two():
     count = 0
-    package = 0
     lines = []
     for line in input():
         if not line:
@@ -120,17 +73,11 @@ def part_two():
         lines.append(eval(line))
     lines.append([[2]])
     lines.append([[6]])
-    #sorted_lines = sorted(lines, key=functools.cmp_to_key(compare2))
-    print(lines)
-    sorted1 = sorted(lines, key=compare_key)
-    print(sorted1)
-    index = 0
-    for l in sorted1:
-        index += 1
-        if l == [[2]]:
-            print(index, 1)
-        if l == [[6]]:
-            print(index, 2)
+    sorted1 = sorted(lines, key=functools.cmp_to_key(compare))
+    count = 1
+    for i, l in enumerate(sorted1):
+        if l == [[2]] or l == [[6]]:
+            count *= (i + 1)
     print("Part 2: ", count)
     assert count == 22000
 

1b15f7db90037c0ba7daa569a885ebed1a4ff9ab
tn82 <tomas.thurin@gmail.com>
2022-12-24 20:29:10 +0100

    AOC-2022 Day24

--- 2022/day24/aoc.py
+++ 2022/day24/aoc.py
@@ -19,8 +19,7 @@ jumps["W"] = (-1, 0)
 jumps["H"] = (0, 0)
 dirs = "SEHNW"
 
-def part_one():
-    count = 0
+def day24():
     grid = {}
     wall = {}
     wall[(1, -1)] = 1
@@ -30,19 +29,18 @@ def part_one():
                 grid[(x, y)] = [c]
             if c == "#":
                 wall[(x, y)] = 1
-    wall_x = 7
-    wall_y = 5
-    wall_x = 121
-    wall_y = 26
-    pos = (1, 0)
+    wall_x = max([x for x, _ in wall.keys()])
+    wall_y = max([y for _, y in wall.keys()])
+    start_position = (1, 0)
     goal = (6, 5)
     goal = (120, 26)
     wall[(goal[0], goal[1] + 1)] = 1
     time = 0
-    path_list = [[pos]]
+    path_list = [start_position]
     goal_list = [1]
+    part_one = 0
+    part_two = 0
     while True:
-        # move time
         grid_tmp = defaultdict(list)
         for coo, vals in grid.items():
             while vals:
@@ -63,46 +61,38 @@ def part_one():
         grid = grid_tmp
 
         time += 1
-        # move pos in an optimal way?
+        # move pos in an optimal way
         path_list_tmp = []
         goal_list_tmp = []
         cache = set()
-        dist = []
-        found_goal = False
         for path, goal_num in zip(path_list, goal_list):
-            if len(path) > 300 and goal_num == 1:
+            if time > 300 and goal_num == 1:
                 continue
-            if len(path) > 600 and goal_num == 2:
+            if time > 600 and goal_num == 2:
                 continue
             for d in dirs:
-                new_pos = path[-1][0] + jumps[d][0], path[-1][1] + jumps[d][1]
+                new_pos = path[0] + jumps[d][0], path[1] + jumps[d][1]
                 if new_pos not in grid and new_pos not in wall:
                     if (new_pos[0], new_pos[1], goal_num) in cache:
                         continue
                     cache.add((new_pos[0], new_pos[1], goal_num))
-                    dist.append(goal[0] - new_pos[0] + goal[1] - new_pos[1])
-                    if goal[0] - new_pos[0] < 0:
-                        print("Error", goal, new_pos)
-                    if goal[1] - new_pos[1] < 0:
-                        print("Error", goal, new_pos)
-                    new_path = deepcopy(path)
-                    new_path.append(new_pos)
-                    path_list_tmp.append(new_path)
-                    if goal_num in (1, 3) and new_pos == goal or goal_num == 2 and new_pos == pos:
-                        print(time, goal_num) # 277 low
+                    path_list_tmp.append(new_pos)
+                    if goal_num in (1, 3) and new_pos == goal or goal_num == 2 and new_pos == start_position:
+                        if goal_num == 1 and not part_one:
+                            part_one = time
                         if goal_num == 3:
+                            part_two = time
+                            print("Part 1: ", part_one)
+                            assert part_one == 277
+
+                            print("Part 2: ", part_two)
+                            assert part_two == 877
                             exit(0)
                         goal_list_tmp.append(goal_num + 1)
                     else:
                         goal_list_tmp.append(goal_num)
         path_list = path_list_tmp
         goal_list = goal_list_tmp
-        print("Time:", time, len(path_list), min(dist), goal_list.count(1), goal_list.count(2), goal_list.count(3))
-    print("Part 1: ", time)
-    assert time == 277
+        print("Time:", time, len(path_list), goal_list.count(1), goal_list.count(2), goal_list.count(3))
 
-    print("Part 2: ", count)
-    assert count == 0
-
-
-part_one()
+day24()

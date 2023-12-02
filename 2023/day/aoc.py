e51c8b969456a18f670a763d3a67ac8c6621dc2e
Tomas Thurin <tomas.thurin@gmail.com>
2023-12-02 11:14:55 +0100

    AOC-2023 Day 2 clean up

--- 2023/day/aoc.py
+++ 2023/day/aoc.py
@@ -5,23 +5,26 @@ def input():
     with open(os.path.join(day_path, "input.txt"), "r") as file:
         return [line.rstrip() for line in file]
 
-def input_test():
+def test():
     with open(os.path.join(day_path, "test.txt"), "r") as file:
         return [line.rstrip() for line in file]
 
 def part_one():
-    sum = 0
-    for line in input():
-        sum += int(line)
+    sums = 0
+    for line in test():
+        sums += int(line)
 
-    print("Part 1: ", sum)
+    print("Part 1: ", sums)
+    #assert(sums == 0)
 
 def part_two():
-    sum = 0
-    for line in input():
-        sum += int(line)
+    sums = 0
+    for line in test():
+        sums += int(line)
 
-    print("Part 2: ", sum)
+    print("Part 2: ", sums)
+    #assert(sums == 0)
+
 
 part_one()
 #part_two()
--- 2023/day02/aoc.py
+++ 2023/day02/aoc.py
@@ -5,53 +5,44 @@ def input():
     with open(os.path.join(day_path, "input.txt"), "r") as file:
         return [line.rstrip() for line in file]
 
-def input_test():
+def test():
     with open(os.path.join(day_path, "test.txt"), "r") as file:
         return [line.rstrip() for line in file]
 
 # only 12 red cubes, 13 green cubes, and 14 blue cubes
 def part_one():
-    games = []
     sums = 0
     for id, line in enumerate(input()):
-        gamenbr, rest = line.split(":")
-        subsets = rest.split(";")
+        _, subsets = line.split(":")
         ok = True
-        for sub in subsets:
-            colors = sub.split(",")
-            for nbrcol in colors:
-                nbr, col = nbrcol.strip().split()
-                if col == "red" and int(nbr) > 12:
+        for subset in subsets.split(";"):
+            for nbr_color in subset.split(","):
+                nbr, color = nbr_color.strip().split()
+                if color == "red" and int(nbr) > 12:
                     ok = False
                     break
-                if col == "green" and int(nbr) > 13:
+                if color == "green" and int(nbr) > 13:
                     ok = False
                     break
-                if col == "blue" and int(nbr) > 14:
+                if color == "blue" and int(nbr) > 14:
                     ok = False
                     break
         if ok:
             sums += id + 1
 
-        games.append(subsets)
-        #sum += int(line)
-
     print("Part 1: ", sums)
+    assert(sums == 2348)
 
 def part_two():
-    games = []
     sums = 0
-    for id, line in enumerate(input()):
-        gamenbr, rest = line.split(":")
-        subsets = rest.split(";")
-        ok = True
+    for _, line in enumerate(input()):
+        _, subsets = line.split(":")
         red_max = 1
         green_max = 1
         blue_max = 1
-        for sub in subsets:
-            colors = sub.split(",")
-            for nbrcol in colors:
-                nbr, col = nbrcol.strip().split()
+        for subset in subsets.split(";"):
+            for nbr_color in subset.split(","):
+                nbr, col = nbr_color.strip().split()
                 if col == "red" and int(nbr) > red_max:
                     red_max = int(nbr)
                 if col == "green" and int(nbr) > green_max:
@@ -60,9 +51,9 @@ def part_two():
                     blue_max = int(nbr)
         sums += red_max * green_max * blue_max
 
-        games.append(subsets)
-        #sum += int(line)
     print("Part 2: ", sums)
+    assert(sums == 76008)
 
-#part_one()
+
+part_one()
 part_two()

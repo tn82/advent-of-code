2338f719c003874f7e1752b299e9e6ade8dd6bc9
Tomas Thurin <tomas.thurin@gmail.com>
2023-12-10 15:06:28 +0100

    AOC-2023 Day 10

--- 2023/day10/aoc.py
+++ 2023/day10/aoc.py
@@ -1,6 +1,4 @@
 import os
-from collections import defaultdict
-
 day_path = os.path.dirname(__file__)
 
 def input():
@@ -11,26 +9,155 @@ def test():
     with open(os.path.join(day_path, "test.txt"), "r") as file:
         return [line.rstrip() for line in file]
 
-def int_list(char_list):
-    return [int(c) for c in char_list]
+def input2():
+    with open(os.path.join(day_path, "input.txt"), "r") as file:
+        return [line.rstrip().replace("FJ", "-|").replace("L7", "-|") for line in file]
+
+def test2():
+    with open(os.path.join(day_path, "test.txt"), "r") as file:
+        return [line.rstrip().replace("FJ", "-|").replace("L7", "-|") for line in file]
+
+
+
+def south_jump(c1, c2):
+    if c1 in ("S", "|", "F", "7") and c2 in ("|", "L", "J"):
+        return True
+    return False
+
+def north_jump(c1, c2):
+    if c1 in ("S", "|", "L", "J") and c2 in ("|", "F", "7"):
+        return True
+    return False
+
+def east_jump(c1, c2):
+    if c1 in ("S", "-", "L", "F") and c2 in ("-", "J", "7"):
+        return True
+    return False
+
+def west_jump(c1, c2):
+    if c1 in ("S", "-", "J", "7") and c2 in ("-", "L", "F"):
+        return True
+    return False
 
 def part_one():
-    sums = 0
-    for i, line in enumerate(test()):
-        sums += int(line)
+    grid = {}
+    start = None
+    for r, line in enumerate(input()):
+        for c, col in enumerate(line):
+            grid[(r, c)] = col
+            if col == "S":
+                start = (r, c)
+    
+    dists = {}
+    dists[start] = 0
+    jump_q = [start]
 
-    print("Part 1: ", sums)
-    #assert(sums == 0)
+    while jump_q:
+        coo = jump_q.pop(0)
+        coo_s = (coo[0] + 1, coo[1])
+        if coo_s not in dists and coo_s in grid and south_jump(grid[coo], grid[coo_s]):
+            dists[coo_s] = dists[coo] + 1
+            jump_q.append(coo_s)
+        coo_s = (coo[0] - 1, coo[1])
+        if coo_s not in dists and coo_s in grid and north_jump(grid[coo], grid[coo_s]):
+            dists[coo_s] = dists[coo] + 1
+            jump_q.append(coo_s)
+        coo_s = (coo[0], coo[1] + 1)
+        if coo_s not in dists and coo_s in grid and east_jump(grid[coo], grid[coo_s]):
+            dists[coo_s] = dists[coo] + 1
+            jump_q.append(coo_s)
+        coo_s = (coo[0], coo[1] - 1)
+        if coo_s not in dists and coo_s in grid and west_jump(grid[coo], grid[coo_s]):
+            dists[coo_s] = dists[coo] + 1
+            jump_q.append(coo_s)
 
+
+    print("Part 1: ", max(dists.values()))
+    assert(max(dists.values()) == 6800)
+
+def printer(grid, dists, c):
+    row = 0
+    for coo in grid:
+        if coo[0] != row:
+            row = coo[0]
+            print()
+        if coo in dists:
+            if c:
+                print(c, end = "")
+            else:
+                print(grid[coo], end = "")
+        else:
+            print(".", end = "")
+    print()
+    print()
+    
 
 def part_two():
+    grid = {}
+    start = None
+    for r, line in enumerate(input()):
+        for c, col in enumerate(line):
+            grid[(r, c)] = col
+            if col == "S":
+                start = (r, c)
+    
+    dists = {}
+    dists[start] = 0
+    jump_q = [start]
+
+    while jump_q:
+        coo = jump_q.pop()
+        coo_s = (coo[0] + 1, coo[1])
+        if coo_s not in dists and coo_s in grid and south_jump(grid[coo], grid[coo_s]):
+            dists[coo_s] = dists[coo] + 1
+            jump_q.append(coo_s)
+            continue
+        coo_s = (coo[0] - 1, coo[1])
+        if coo_s not in dists and coo_s in grid and north_jump(grid[coo], grid[coo_s]):
+            dists[coo_s] = dists[coo] + 1
+            jump_q.append(coo_s)
+            continue
+        coo_s = (coo[0], coo[1] + 1)
+        if coo_s not in dists and coo_s in grid and east_jump(grid[coo], grid[coo_s]):
+            dists[coo_s] = dists[coo] + 1
+            jump_q.append(coo_s)
+            continue
+        coo_s = (coo[0], coo[1] - 1)
+        if coo_s not in dists and coo_s in grid and west_jump(grid[coo], grid[coo_s]):
+            dists[coo_s] = dists[coo] + 1
+            jump_q.append(coo_s)
+            continue
+
+    grid_replace = {}
+    for r, line in enumerate(test2()):
+        for c, col in enumerate(line):
+            grid_replace[(r, c)] = col
+
     sums = 0
-    for i, line in enumerate(test()):
-        sums += int(line)
+    enclosed = {}
+    for coo in grid:
+        if coo in dists:
+            continue
+        coo_s = coo
+        count_crosses = 0
+        while coo_s:
+            coo_s = (coo_s[0], coo_s[1] + 1)
+            if coo_s not in grid:
+                coo_s = None
+            #elif coo_s in dists and grid_replace[coo_s] in ("|", "F", "J", "7", "L", "S"):
+            #    count_crosses += 1
+            elif coo_s in dists and grid[coo_s] in ("|", "J", "L", "S"):
+                count_crosses += 1
+        if count_crosses % 2 != 0:
+            sums += 1
+            enclosed[coo] = 1
+    
+    printer(grid, dists, None)
 
+    printer(grid_replace, dists, None)
     print("Part 2: ", sums)
-    #assert(sums == 0)
+    assert(sums == 6800)
 
 
-part_one()
-#part_two()
+#part_one()
+part_two()

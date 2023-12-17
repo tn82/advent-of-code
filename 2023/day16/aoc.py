c2c673996f506e822dc548859bc41440ac6dfbe6
Tomas Thurin <tomas.thurin@gmail.com>
2023-12-17 12:15:15 +0100

    AOC-2023 Day 17

--- 2023/day16/aoc.py
+++ 2023/day16/aoc.py
@@ -1,5 +1,4 @@
 import os
-from collections import defaultdict
 
 day_path = os.path.dirname(__file__)
 
@@ -10,98 +9,6 @@ def input():
 def test():
     with open(os.path.join(day_path, "test.txt"), "r") as file:
         return [line.rstrip() for line in file]
-
-def int_list(char_list):
-    return [int(c) for c in char_list]
-
-def grid_printer(grid):
-    row = 0
-    for coo, v in grid.items():
-        r, c = coo
-        if r != row:
-            print()
-            row = r
-        print(v, end = "")
-
-    print()
-    print()
-
-def part_one():
-    sums = 0
-    grid = {}
-    for i, line in enumerate(input()):
-        for j, v in enumerate(line):
-            grid[(i, j)] = v
-
-    litt = {}
-    q = [("R", 0, 0)]
-    while q:
-        d, x, y = q.pop()
-        if (x, y) not in grid:
-            continue
-        if (x, y, d) in litt:
-            continue
-        litt[(x, y, d)] = 1
-        val = grid[(x, y)]
-        if d == "L":
-            if val == ".":
-                q.append((d, x, y - 1))
-            elif val == "-":
-                q.append((d, x, y - 1))
-            elif val == "/":
-                q.append(("D", x + 1, y))
-            elif val == "T":
-                q.append(("U", x - 1, y))
-            elif val == "|":
-                q.append(("D", x + 1, y))
-                q.append(("U", x - 1, y))
-
-        elif d == "R":
-            if val == ".":
-                q.append((d, x, y + 1))
-            elif val == "-":
-                q.append((d, x, y + 1))
-            elif val == "/":
-                q.append(("U", x - 1, y))
-            elif val == "T":
-                q.append(("D", x + 1, y))
-            elif val == "|":
-                q.append(("D", x + 1, y))
-                q.append(("U", x - 1, y))
-
-        elif d == "D":
-            if val == ".":
-                q.append((d, x + 1, y))
-            elif val == "-":
-                q.append(("L", x, y - 1))
-                q.append(("R", x, y + 1))
-            elif val == "/":
-                q.append(("L", x, y - 1))
-            elif val == "T":
-                q.append(("R", x, y + 1))
-            elif val == "|":
-                q.append((d, x + 1, y))
-
-        elif d == "U":
-            if val == ".":
-                q.append((d, x - 1, y))
-            elif val == "-":
-                q.append(("L", x, y - 1))
-                q.append(("R", x, y + 1))
-            elif val == "/":
-                q.append(("R", x, y + 1))
-            elif val == "T":
-                q.append(("L", x, y - 1))
-            elif val == "|":
-                q.append((d, x - 1, y))
-
-
-    litt_u = {}
-    for k, v in litt.items():
-        litt_u[(k[0], k[1])] = 1
-    print("Part 1: ", sum(litt_u.values()))
-    #assert(sums == 7185)
-
 
 def litter(grid, sx, sy, d):
     litt = {}
@@ -120,12 +27,12 @@ def litter(grid, sx, sy, d):
             elif val == "-":
                 q.append((d, x, y - 1))
             elif val == "/":
-                q.append(("D", x + 1, y))
+                q.append(("D", x - 1, y))
             elif val == "T":
-                q.append(("U", x - 1, y))
+                q.append(("U", x + 1, y))
             elif val == "|":
-                q.append(("D", x + 1, y))
-                q.append(("U", x - 1, y))
+                q.append(("D", x - 1, y))
+                q.append(("U", x + 1, y))
 
         elif d == "R":
             if val == ".":
@@ -133,16 +40,16 @@ def litter(grid, sx, sy, d):
             elif val == "-":
                 q.append((d, x, y + 1))
             elif val == "/":
-                q.append(("U", x - 1, y))
+                q.append(("U", x + 1, y))
             elif val == "T":
-                q.append(("D", x + 1, y))
+                q.append(("D", x - 1, y))
             elif val == "|":
-                q.append(("D", x + 1, y))
-                q.append(("U", x - 1, y))
+                q.append(("D", x - 1, y))
+                q.append(("U", x + 1, y))
 
         elif d == "D":
             if val == ".":
-                q.append((d, x + 1, y))
+                q.append((d, x - 1, y))
             elif val == "-":
                 q.append(("L", x, y - 1))
                 q.append(("R", x, y + 1))
@@ -151,11 +58,11 @@ def litter(grid, sx, sy, d):
             elif val == "T":
                 q.append(("R", x, y + 1))
             elif val == "|":
-                q.append((d, x + 1, y))
+                q.append((d, x - 1, y))
 
         elif d == "U":
             if val == ".":
-                q.append((d, x - 1, y))
+                q.append((d, x + 1, y))
             elif val == "-":
                 q.append(("L", x, y - 1))
                 q.append(("R", x, y + 1))
@@ -164,13 +71,27 @@ def litter(grid, sx, sy, d):
             elif val == "T":
                 q.append(("L", x, y - 1))
             elif val == "|":
-                q.append((d, x - 1, y))
-
-
+                q.append((d, x + 1, y))
     litt_u = {}
-    for k, v in litt.items():
+    for k, _ in litt.items():
         litt_u[(k[0], k[1])] = 1
     return sum(litt_u.values())
+
+def part_one():
+    grid = {}
+    xmax = 0
+    ymax = 0
+    for i, line in enumerate(input()):
+        for j, v in enumerate(line):
+            grid[(i, j)] = v
+            if i > xmax:
+                xmax = i
+            if j > ymax:
+                ymax = j
+
+    litt = litter(grid, xmax, 0, "L")
+    print("Part 1: ", litt)
+    assert(litt == 7185)
 
 def part_two():
     grid = {}
@@ -203,5 +124,5 @@ def part_two():
     #assert(sums == 7185)
 
 
-#part_one()
+part_one()
 part_two()
---
+++ 2023/day17/aoc.py
@@ -0,0 +1,167 @@
+import os
+
+day_path = os.path.dirname(__file__)
+
+def input():
+    with open(os.path.join(day_path, "input.txt"), "r") as file:
+        return [line.rstrip() for line in file]
+
+def test():
+    with open(os.path.join(day_path, "test.txt"), "r") as file:
+        return [line.rstrip() for line in file]
+
+def part_one():
+    grid = {}
+    xmax = 0
+    ymax = 0
+    for i, line in enumerate(reversed(input())):
+        for j, v in enumerate(line):
+            grid[(i, j)] = int(v)
+            if i > xmax:
+                xmax = i
+            if j > ymax:
+                ymax = j
+
+    q = [[(xmax, 0, "x", 0)]]
+    best_grid = {}
+    best_global = 0
+    
+    x = xmax
+    y = 0
+    while True:
+        x -= 1
+        best_global += grid[(x, y)]
+        if x == 0 and y == ymax:
+            break
+        y += 1
+        best_global += grid[(x, y)]
+        if x == 0 and y == ymax:
+            break
+    best_global = 850
+
+    while q:
+        path = q.pop()
+        sx, sy, sd, cost = path[-1]
+        directions = ("L", "R") if sd in ("U", "D") else ("U", "D")
+        for d in directions:        
+            if d == "L":
+                jx = 0
+                jy = -1
+            elif d == "R":
+                jx = 0
+                jy = 1
+            elif d == "D":
+                jx = -1
+                jy = 0
+            elif d == "U":
+                jx = 1
+                jy = 0
+        
+            for i in range(1, 4):
+                if not (sx + jx * i, sy + jy * i) in grid:
+                    continue
+                #if (sx + jx * i, sy + jy * i, d) in path:
+                #    continue
+                weight = 0
+                for iw in range(1, i + 1):
+                    weight += grid[(sx + jx * iw, sy + jy * iw)]
+                if (sx + jx * i, sy + jy * i, d) in best_grid and best_grid[(sx + jx * i, sy + jy * i, d)] < cost + weight:
+                    continue
+                if cost + weight + sx + jx * i + (ymax - sy + jy * i) > best_global + 1:
+                    continue
+                best_grid[(sx + jx * i, sy + jy * i, d)] = cost + weight
+                new_path = path + [(sx + jx * i, sy + jy * i, d, cost + weight)]
+                q.append(new_path)
+                if sx + jx * i == 0 and sy + jy * i == ymax:
+                    best_global = cost + weight
+                    print(len(new_path), len(q), cost + weight)
+
+    #shortest_path_length = networkx.dijkstra_path_length(graph, (xmax, 0, "D"), (0, ymax, "L"))
+    #shortest_path = networkx.shortest_path(graph, (xmax, 0), (0, ymax), weight="yes")
+
+
+    print("Part 1: ", 0)
+    #assert(sums == 847) # high 848 off by one?
+
+def part_two():
+    grid = {}
+    xmax = 0
+    ymax = 0
+    for i, line in enumerate(reversed(input())):
+        for j, v in enumerate(line):
+            grid[(i, j)] = int(v)
+            if i > xmax:
+                xmax = i
+            if j > ymax:
+                ymax = j
+
+    #graph = networkx.DiGraph()
+    #litt = defaultdict(set)
+    paths = [[(xmax, 0, "x", 0)]]
+    q = [[(xmax, 0, "x", 0)]]
+    best_grid = {}
+    best_global = 0
+    
+    x = xmax
+    y = 0
+    while True:
+        for iw in range(1, 5):
+            best_global += grid[(x - iw, y)]
+        x -= 4
+        if x == 0 and y == ymax:
+            break
+        for iw in range(1, 5):
+            best_global += grid[(x, y + iw)]
+        y += 4
+        if x == 0 and y == ymax:
+            break
+    while q:
+        path = q.pop()
+        sx, sy, sd, cost = path[-1]
+        for d in ("L", "R", "D", "U"):
+            if d == sd:
+                continue
+            if sd == "L" and d == "R":
+                continue
+            if sd == "R" and d == "L":
+                continue
+            if sd == "U" and d == "D":
+                continue
+            if sd == "D" and d == "U":
+                continue
+        
+            if d == "L":
+                jx = 0
+                jy = -1
+            elif d == "R":
+                jx = 0
+                jy = 1
+            elif d == "D":
+                jx = -1
+                jy = 0
+            elif d == "U":
+                jx = 1
+                jy = 0
+        
+            for i in range(4, 11):
+                if not (sx + jx * i, sy + jy * i) in grid:
+                    continue
+                #if (sx + jx * i, sy + jy * i, d) in path:
+                #    continue
+                weight = 0
+                for iw in range(1, i + 1):
+                    weight += grid[(sx + jx * iw, sy + jy * iw)]
+                if (sx + jx * i, sy + jy * i, d) in best_grid and best_grid[(sx + jx * i, sy + jy * i, d)] <= cost + weight:
+                    continue
+                if cost + weight + sx + jx * i + (ymax - sy + jy * i) > best_global + 1:
+                    continue
+                best_grid[(sx + jx * i, sy + jy * i, d)] = cost + weight
+                new_path = path + [(sx + jx * i, sy + jy * i, d, cost + weight)]
+                q.append(new_path)
+                if sx + jx * i == 0 and sy + jy * i == ymax:
+                    best_global = cost + weight
+                    print(len(new_path), len(q), cost + weight)
+
+
+part_one()
+#part_two()

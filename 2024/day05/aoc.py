3bc31d795d804e08fb2526ce8a822dd6e0330394
Tomas Thurin <tomas.thurin@gmail.com>
2024-12-05 11:10:32 +0100

    AOC-2024 Day05 clean up

--- 2024/day05/aoc.py
+++ 2024/day05/aoc.py
@@ -3,32 +3,33 @@ from collections import defaultdict
 
 day_path = os.path.dirname(__file__)
 
+
 def input():
     with open(os.path.join(day_path, "input.txt"), "r") as file:
         return [line.rstrip() for line in file]
+
 
 def test():
     with open(os.path.join(day_path, "test.txt"), "r") as file:
         return [line.rstrip() for line in file]
 
+
 def input2():
     with open(os.path.join(day_path, "input2.txt"), "r") as file:
         return [line.rstrip() for line in file]
 
+
 def test2():
     with open(os.path.join(day_path, "test2.txt"), "r") as file:
         return [line.rstrip() for line in file]
-    
-def int_list(char_list):
-    return [int(c) for c in char_list]
 
-import copy
 
 def findex(my_list, target):
     try:
         return my_list.index(target)
     except ValueError:
         return -1
+
 
 def part_one():
     sums = 0
@@ -42,20 +43,17 @@ def part_one():
         inst.append([int(c) for c in line.split(",")])
 
     for ins in inst:
-        ins_ok = True
         for ord in order:
             f = findex(ins, ord[0])
             s = findex(ins, ord[1])
             if f != -1 and s != -1 and f > s:
-                ins_ok = False
                 break
-        if ins_ok:
-            sums += ins[int((len(ins)-1)/2)]
-
-
+        else:
+            # only executed if the inner loop did NOT break
+            sums += ins[int((len(ins) - 1) / 2)]
 
     print("Part 1: ", sums)
-    assert(sums == 4774)
+    assert sums == 4774
 
 
 def part_two():
@@ -71,67 +69,32 @@ def part_two():
 
     bad = []
     for ins in inst:
-        ins_ok = True
         for ord in order:
             f = findex(ins, ord[0])
             s = findex(ins, ord[1])
             if f != -1 and s != -1 and f > s:
-                ins_ok = False
                 break
-        if not ins_ok:
-            bad.append(ins)
+        else:
+            # only executed if the inner loop did NOT break
+            continue
+        bad.append(ins)
 
     for ins in bad:
-        for ord in order:
-            f = findex(ins, ord[0])
-            s = findex(ins, ord[1])
-            if f != -1 and s != -1 and f > s:
-                ins[f], ins[s] = ins[s], ins[f]
+        updated = 1
+        while updated:
+            updated = 0
+            for ord in order:
+                f = findex(ins, ord[0])
+                s = findex(ins, ord[1])
+                if f != -1 and s != -1 and f > s:
+                    updated += 1
+                    ins[f], ins[s] = ins[s], ins[f]
 
     for ins in bad:
-        for ord in order:
-            f = findex(ins, ord[0])
-            s = findex(ins, ord[1])
-            if f != -1 and s != -1 and f > s:
-                ins[f], ins[s] = ins[s], ins[f]
-    for ins in bad:
-        for ord in order:
-            f = findex(ins, ord[0])
-            s = findex(ins, ord[1])
-            if f != -1 and s != -1 and f > s:
-                ins[f], ins[s] = ins[s], ins[f]
-    for ins in bad:
-        for ord in order:
-            f = findex(ins, ord[0])
-            s = findex(ins, ord[1])
-            if f != -1 and s != -1 and f > s:
-                ins[f], ins[s] = ins[s], ins[f]
-
-    for ins in bad:
-        for ord in order:
-            f = findex(ins, ord[0])
-            s = findex(ins, ord[1])
-            if f != -1 and s != -1 and f > s:
-                ins[f], ins[s] = ins[s], ins[f]
-    for ins in bad:
-        for ord in order:
-            f = findex(ins, ord[0])
-            s = findex(ins, ord[1])
-            if f != -1 and s != -1 and f > s:
-                ins[f], ins[s] = ins[s], ins[f]
-    for ins in bad:
-        ins_ok = True
-        for ord in order:
-            f = findex(ins, ord[0])
-            s = findex(ins, ord[1])
-            if f != -1 and s != -1 and f > s:
-                ins_ok = False
-                break
-        if ins_ok:
-            sums += ins[int((len(ins)-1)/2)]
+        sums += ins[int((len(ins) - 1) / 2)]
 
     print("Part 2: ", sums)
-    assert(sums == 6004)
+    assert sums == 6004
 
 
 part_one()

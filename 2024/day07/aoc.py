0d1ca52b55ab084a4ed91c915f9dd7e2247e5cc4
Tomas Thurin <tomas.thurin@gmail.com>
2024-12-07 19:01:42 +0100

    AOC-2024 Day07 refactor

--- 2024/day07/aoc.py
+++ 2024/day07/aoc.py
@@ -3,65 +3,22 @@ from collections import defaultdict
 
 day_path = os.path.dirname(__file__)
 
+
 def input():
     with open(os.path.join(day_path, "input.txt"), "r") as file:
         return [line.rstrip() for line in file]
+
 
 def test():
     with open(os.path.join(day_path, "test.txt"), "r") as file:
         return [line.rstrip() for line in file]
 
+
 def int_list(char_list):
     return [int(c) for c in char_list]
 
-import copy
 
-def reccu(facit, vs, add, curr_sum=0):
-    a = vs.pop(0)
-    b = vs[0]
-    if add:
-        curr_sum = curr_sum + b
-    if not add:
-        curr_sum = curr_sum * b
-    if len(vs) == 1:
-        return 1 if curr_sum == facit else 0
-    t1 = reccu(facit, vs.copy(), True, curr_sum)
-    t2 = reccu(facit, vs.copy(), False, curr_sum)
-
-    return t1 or t2
-
-
-
-def part_one():
-    sums = 0
-    res = []
-    vals = []
-    for i, line in enumerate(input()):
-        r, v = line.split(":")
-        res.append(int(r))
-        v = v.split(" ")
-        vals2 = []
-        for i in v:
-            if i and i != " ":
-                vals2.append(int(i))
-        vals.append(vals2)
-    
-    for r, vs in zip(res, vals):
-        t1 = reccu(r, vs.copy(), True, vs[0])
-        t2 = reccu(r, vs.copy(), False, vs[0])
-
-        if t1 or t2:
-            sums += r
-
-    
-    
-    
-
-    print("Part 1: ", sums)
-    assert(sums == 1399219271639)
-
-def reccu2(facit, vs, add, curr_sum=0):
-    vs.pop(0)
+def reccu(facit, vs, add, use_concat, curr_sum=0):
     b = vs[0]
     if add == 1:
         curr_sum = curr_sum + b
@@ -71,14 +28,14 @@ def reccu2(facit, vs, add, curr_sum=0):
         curr_sum = int(str(curr_sum) + str(b))
     if len(vs) == 1:
         return 1 if curr_sum == facit else 0
-    t1 = reccu2(facit, vs.copy(), 1, curr_sum)
-    t2 = reccu2(facit, vs.copy(), 2, curr_sum)
-    t3 = reccu2(facit, vs.copy(), 3, curr_sum)
+    t1 = reccu(facit, vs[1:], 1, use_concat, curr_sum)
+    t2 = reccu(facit, vs[1:], 2, use_concat, curr_sum)
+    t3 = reccu(facit, vs[1:], 3, use_concat, curr_sum) if use_concat else 0
 
     return t1 or t2 or t3
 
-def part_two():
-    sums = 0
+
+def day07():
     res = []
     vals = []
     for i, line in enumerate(input()):
@@ -90,18 +47,29 @@ def part_two():
             if i and i != " ":
                 vals2.append(int(i))
         vals.append(vals2)
-    
+
+    part1 = 0
     for r, vs in zip(res, vals):
-        t1 = reccu2(r, vs.copy(), 1, vs[0])
-        t2 = reccu2(r, vs.copy(), 2, vs[0])
-        t3 = reccu2(r, vs.copy(), 3, vs[0])
+        t1 = reccu(r, vs[1:], 1, False, vs[0])
+        t2 = reccu(r, vs[1:], 2, False, vs[0])
+
+        if t1 or t2:
+            part1 += r
+
+    print("Part 1: ", part1)
+    assert part1 == 1399219271639
+
+    part2 = 0
+    for r, vs in zip(res, vals):
+        t1 = reccu(r, vs[1:], 1, True, vs[0])
+        t2 = reccu(r, vs[1:], 2, True, vs[0])
+        t3 = reccu(r, vs[1:], 3, True, vs[0])
 
         if t1 or t2 or t3:
-            sums += r
+            part2 += r
 
-    print("Part 2: ", sums)
-    assert(sums == 275791737999003)
+    print("Part 2: ", part2)
+    assert part2 == 275791737999003
 
 
-part_one()
-part_two()
+day07()
